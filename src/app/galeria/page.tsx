d0093629b7c4bfe9c574a245782c36bb514f815e
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2025-03-04 13:55:00 -0300

    Componente ImageCard e Página Galeria

--- src/app/galeria/page.tsx
+++ src/app/galeria/page.tsx
@@ -1,9 +1,17 @@
-import { Template } from "@/components"
+import { Template, ImageCard } from "@/components"
 
 export default function GaleriaPage(){
   return(
     <Template>
-      <h1>Galeria</h1>
+      <section className="grid grid-cols-4 gap-8">
+        <ImageCard nome='Montanha' tamanho='10MB' dataUpload='01/01/2025' src='https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=1024x1024&w=0&k=20&c=z8_rWaI8x4zApNEEG9DnWlGXyDIXe-OmsAyQ5fGPVV8='/>
+        <ImageCard nome='Montanha' tamanho='10MB' dataUpload='01/01/2025' src='https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=1024x1024&w=0&k=20&c=z8_rWaI8x4zApNEEG9DnWlGXyDIXe-OmsAyQ5fGPVV8='/>
+        <ImageCard nome='Montanha' tamanho='10MB' dataUpload='01/01/2025' src='https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=1024x1024&w=0&k=20&c=z8_rWaI8x4zApNEEG9DnWlGXyDIXe-OmsAyQ5fGPVV8='/>
+        <ImageCard nome='Montanha' tamanho='10MB' dataUpload='01/01/2025' src='https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=1024x1024&w=0&k=20&c=z8_rWaI8x4zApNEEG9DnWlGXyDIXe-OmsAyQ5fGPVV8='/>
+        <ImageCard nome='Montanha' tamanho='10MB' dataUpload='01/01/2025' src='https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=1024x1024&w=0&k=20&c=z8_rWaI8x4zApNEEG9DnWlGXyDIXe-OmsAyQ5fGPVV8='/>
+        <ImageCard nome='Montanha' tamanho='10MB' dataUpload='01/01/2025' src='https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=1024x1024&w=0&k=20&c=z8_rWaI8x4zApNEEG9DnWlGXyDIXe-OmsAyQ5fGPVV8='/>
+        <ImageCard nome='Montanha' tamanho='10MB' dataUpload='01/01/2025' src='https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=1024x1024&w=0&k=20&c=z8_rWaI8x4zApNEEG9DnWlGXyDIXe-OmsAyQ5fGPVV8='/>
+      </section>
     </Template>
   )
 }
