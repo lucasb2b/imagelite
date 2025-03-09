b0cf0023ac0ea92865460b55849b02ce5210c1a8
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2025-03-09 11:50:51 -0300

    Finalizando a tela de Pesquisa de imagens

--- src/app/galeria/page.tsx
+++ src/app/galeria/page.tsx
@@ -11,11 +11,13 @@ export default function GaleriaPage(){
   const [images, setImages] = useState<Image[]>([]);
   const [query, setQuery] = useState<string>('');
   const [extension, setExtension] = useState<string>();
+  const [loading, setLoading] = useState<boolean>(false);
 
   async function searchImages(){
-    console.log("Valor digitado: ", query);
+    setLoading(true);
     const result = await useService.buscar(query, extension);
     setImages(result);
+    setLoading(false);
   }
 
   function renderImageCard(image: Image){
@@ -35,7 +37,7 @@ export default function GaleriaPage(){
   }
 
   return(
-    <Template>
+    <Template loading={loading}>
 
       <section className="flex flex-col items-center justify-center my-5">
         <div className="flex space-x-4">
@@ -49,8 +51,8 @@ export default function GaleriaPage(){
             <option value="JPEG">JPEG</option>
             <option value="GIF">GIF</option>
           </select>
-          <button className="bg-blue-500 text-white px-4 py-2 rounded-lg" onClick={searchImages}>Search</button>
-          <button className="bg-yellow-500 text-white px-4 py-2 rounded-lg">Add new</button>
+          <button className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-300" onClick={searchImages}>Search</button>
+          <button className="bg-yellow-500 text-white px-4 py-2 rounded-lg hover:bg-yellow-300">Add new</button>
         </div>
       </section>
 
