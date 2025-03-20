10077edfe35bf75a78cf287554aa2119c62cbe9d
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2025-03-20 20:54:30 -0300

    Capturando os valores dos campos via Formik

--- src/app/formulario/page.tsx
+++ src/app/formulario/page.tsx
@@ -1,26 +1,45 @@
+'use client'
+
 import { InputText, Template, Button } from "@/components";
-import Link from 'next/link'
+import Link from "next/link";
+import { useFormik } from "formik";
+
+interface FormProps {
+  name: string;
+  tags: string;
+  file: any;
+}
+
+const formScheme: FormProps = { name: '', tags: '', file: ''}
 
 export default function FormularioPage() {
+
+  const formik = useFormik<FormProps>({
+    initialValues: formScheme,
+    onSubmit: (dados: FormProps) => {
+      console.log(dados)
+    }
+  })
+
   return (
     <Template>
       <section className="flex flex-col items-center justify-center my-5">
         <h5 className="mt-3 mb-10 text-3xl font-extrabold tracking-tight text-gray-900">
           Nova Imagem
         </h5>
-        <form>
+        <form onSubmit={formik.handleSubmit}>
           <div className="grid grid-cols-1">
             <label className="block text-sm font-medium leading-6 text-gray-700">
               Name: *
             </label>
-            <InputText placeholder="type the image's name" />
+            <InputText id="name" onChange={formik.handleChange} placeholder="type the image's name" />
           </div>
 
           <div className="mt-5 grid grid-cols-1">
             <label className="block text-sm font-medium leading-6 text-gray-700">
               Tags: *
             </label>
-            <InputText placeholder="type the tags comma separated" />
+            <InputText id="tags" onChange={formik.handleChange} placeholder="type the tags comma separated" />
           </div>
 
           <div className="mt-5 grid grid-cols-1">
@@ -54,9 +73,17 @@ export default function FormularioPage() {
             </div>
           </div>
           <div className="mt-5 flex items-center justify-end gap-x-4">
-            <Button style="bg-blue-500 hover:bg-blue-300" type="submit" label="Save"/>
+            <Button
+              style="bg-blue-500 hover:bg-blue-300"
+              type="submit"
+              label="Save"
+            />
             <Link href="/galeria">
-              <Button style="bg-red-500 hover:bg-red-300" type="button" label="Cancel" />
+              <Button
+                style="bg-red-500 hover:bg-red-300"
+                type="button"
+                label="Cancel"
+              />
             </Link>
           </div>
         </form>
