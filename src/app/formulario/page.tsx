a8601a1a5329137282c3a2bdf3cdc0fdafd9b9f8
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2025-03-23 12:15:58 -0300

    Resetando o formulário para o estado inicial

--- src/app/formulario/page.tsx
+++ src/app/formulario/page.tsx
@@ -15,6 +15,8 @@ interface FormProps {
 const formScheme: FormProps = { name: "", tags: "", file: "" };
 
 export default function FormularioPage() {
+
+  const [loading, setLoading] = useState<boolean>(false);
   const [imagePreview, setImagePreview] = useState<string>();
   const service = useImageService();
 
@@ -24,6 +26,8 @@ export default function FormularioPage() {
   });
 
   async function handleSubmit(dados: FormProps){
+    setLoading(true);
+
     const formData = new FormData();
     formData.append("file", dados.file);
     formData.append("name", dados.name);
@@ -33,6 +37,8 @@ export default function FormularioPage() {
     
     formik.resetForm();
     setImagePreview('');
+
+    setLoading(false);
   }
 
   function onFileUpload(event: React.ChangeEvent<HTMLInputElement>) {
@@ -45,7 +51,7 @@ export default function FormularioPage() {
   }
 
   return (
-    <Template>
+    <Template loading={loading}>
       <section className="flex flex-col items-center justify-center my-5">
         <h5 className="mt-3 mb-10 text-3xl font-extrabold tracking-tight text-gray-900">
           Nova Imagem
@@ -58,6 +64,7 @@ export default function FormularioPage() {
             <InputText
               id="name"
               onChange={formik.handleChange}
+              value={formik.values.name}
               placeholder="type the image's name"
             />
           </div>
@@ -69,6 +76,7 @@ export default function FormularioPage() {
             <InputText
               id="tags"
               onChange={formik.handleChange}
+              value={formik.values.tags}
               placeholder="type the tags comma separated"
             />
           </div>
