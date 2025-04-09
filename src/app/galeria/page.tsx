fa458e8f6ae5c866bf81107b1196659033459b3c
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2025-04-08 21:48:19 -0300

    Implementação do Login de usuário

--- src/app/galeria/page.tsx
+++ src/app/galeria/page.tsx
@@ -8,7 +8,7 @@ import {
   useNotification,
 } from "@/components";
 import { Image } from "@/resources/image/image.resource";
-import { useImageService } from "@/resources/image/image.service";
+import { useImageService } from "@/resources";
 import { useState } from "react";
 import Link from "next/link";
 
