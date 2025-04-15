629a4dde50767d0798093f8803064f8a043a3958
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2025-04-14 21:29:43 -0300

    Enviando o token Jwt via front end

--- src/resources/image/image.service.ts
+++ src/resources/image/image.service.ts
@@ -1,21 +1,32 @@
-import { Image } from "./image.resource"
+import { Image } from "./image.resource";
+import { useAuth } from "@/resources";
 
 class ImageService {
-  baseURL: string = "http://localhost:8080/v1/images"
+  baseURL: string = "http://localhost:8080/v1/images";
+  auth = useAuth();
 
-  async buscar(query: string = "", extension: string = ""): Promise<Image[]>{
-    const url = `${this.baseURL}?query=${query}&extension=${extension}`
-    const response = await fetch(url);
+  async buscar(query: string = "", extension: string = ""): Promise<Image[]> {
+    const userSession = this.auth.getUserSession();
+    const url = `${this.baseURL}?query=${query}&extension=${extension}`;
+    const response = await fetch(url, {
+      headers: {
+        "Authorization": `Bearer ${userSession?.accessToken}`,
+      },
+    });
     return await response.json();
   }
 
-  async salvar(dados: FormData) : Promise<string>{
+  async salvar(dados: FormData): Promise<string> {
+    const userSession = this.auth.getUserSession();
     const response = await fetch(this.baseURL, {
-      method: 'POST',
-      body: dados
-    })
+      method: "POST",
+      body: dados,
+      headers: {
+        "Authorization": `Bearer ${userSession?.accessToken}`,
+      },
+    });
 
-    return response.headers.get('location') ?? ''
+    return response.headers.get("location") ?? "";
   }
 }
 
